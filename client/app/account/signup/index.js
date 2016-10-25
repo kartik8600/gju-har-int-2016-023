aecaeb9628fd2e5b2517b85bbfffe1ee9bfed4a6
Kartik <kartik860@gmail.com>
2016-10-25 07:41:45 +0530

    2nd commit

---
+++ client/app/account/signup/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import angular from 'angular';
+import SignupController from './signup.controller';
+
+export default angular.module('gjuHarInt2016023App.signup', [])
+  .controller('SignupController', SignupController)
+  .name;
